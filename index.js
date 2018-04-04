e3aaadaf15faef20560540e50b622d9533a95340
Alexander Roberts <detrohutt@gmail.com>
2018-04-03 23:45:57 -0500

    Handle array as input and add more tests

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-export default (obj, ...keys) =>
-  JSON.parse(
-    JSON.stringify(
-      Object.assign({}, obj),
-      (k, v) => (keys.indexOf(k) !== -1 ? undefined : v)
-    )
-  )
+const strip = (input, ...keys) =>
+  typeof input !== 'object'
+    ? input
+    : JSON.parse(
+        JSON.stringify(Object.assign({}, input), (k, v) => (keys.indexOf(k) !== -1 ? undefined : v))
+      );
+
+export default (input, ...keys) =>
+  Array.isArray(input) ? input.map(v => strip(v, ...keys)) : strip(input, ...keys);
--- stripper.test.js
+++ stripper.test.js
@@ -1,4 +1,4 @@
-import strip from './index'
+import strip from './index';
 
 test('strips a single key deeply', () => {
   const obj = {
@@ -6,16 +6,16 @@ test('strips a single key deeply', () => {
       keep: true,
       strip: true
     }
-  }
+  };
 
   const expected = {
     test: {
       keep: true
     }
-  }
+  };
 
-  expect(strip(obj, 'strip')).toEqual(expected)
-})
+  expect(strip(obj, 'strip')).toEqual(expected);
+});
 
 test('creates a new object without mutating original', () => {
   const originalShape = {
@@ -23,17 +23,73 @@ test('creates a new object without mutating original', () => {
       keep: true,
       strip: true
     }
-  }
+  };
 
   const obj = {
     test: {
       keep: true,
       strip: true
     }
-  }
+  };
 
-  const newObj = strip(obj, 'strip')
+  const newObj = strip(obj, 'strip');
 
-  expect(obj).toEqual(originalShape)
-  expect(obj).not.toBe(newObj)
+  expect(obj).toEqual(originalShape);
+  expect(obj).not.toBe(newObj);
+});
+
+test('strips fields from an object inside an inner array', () => {
+  const obj = {
+    arr: [
+      {
+        str: 'inner1',
+        bad: 'stripme'
+      },
+      {
+        str: 'inner2',
+        good: 'stillhere'
+      }
+    ]
+  };
+
+  const expected = {
+    arr: [
+      {
+        str: 'inner1'
+      },
+      {
+        str: 'inner2',
+        good: 'stillhere'
+      }
+    ]
+  };
+
+  const actual = strip(obj, 'bad');
+
+  expect(actual).toEqual(expected);
+});
+
+test('handles array as input', () => {
+  const input = [
+    {
+      good: true,
+      bad: true
+    },
+    true,
+    "test",
+    123
+  ]
+
+  const expected = [
+    {
+      good: true
+    },
+    true,
+    "test",
+    123
+  ]
+
+  const actual = strip(input, 'bad')
+
+  expect(actual).toEqual(expected)
 })
